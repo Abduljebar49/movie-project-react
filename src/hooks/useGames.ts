19e371f96bddef28549271e05459e2e2083653e2
Abduljebar <Abduljebar49@gmail.com>
2023-12-02 12:22:29 +0300

    filter games by genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import useData from "./useData";
+import { Genre } from "./useGenre";
 
 export interface Platform {
   id: number;
@@ -13,11 +14,10 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
-  metacritic:number;
+  metacritic: number;
 }
 
-
-
-const useGames = () => useData<Game>('/games');
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } },[selectedGenre?.id]);
 
 export default useGames;
