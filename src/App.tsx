4661cc21ce29cc4648dd32db4b6a61347bfa9ac2
Abduljebar <Abduljebar49@gmail.com>
2023-12-02 12:45:40 +0300

    filter by platform done

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,44 @@
-import { Grid, GridItem,Show } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./Components/NavBar";
 import GameGrid from "./Components/GameGrid";
 import GenreList from "./Components/GenreList";
 import { Genre } from "./hooks/useGenre";
 import { useState } from "react";
 import PlatformSelected from "./Components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
-
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <>
-      <Grid templateAreas={{
-        base: `"nav" "main"`,
-        lg:`"nav nav" "aside main"`
-      }}
-      templateColumns={{
-        base: '1fr',
-        lg: '200px 1fr'
-      }}
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
       >
         <GridItem area="nav">
-          <NavBar  />
+          <NavBar />
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" paddingX={5}>
-            <GenreList onSelectedGenre={setSelectedGenre} selectedGenre={selectedGenre} />
+            <GenreList
+              onSelectedGenre={setSelectedGenre}
+              selectedGenre={selectedGenre}
+            />
           </GridItem>
         </Show>
         <GridItem area="main">
-          <PlatformSelected />
-          <GameGrid selectedGenre={selectedGenre} />
+          <PlatformSelected selectedPlatform={selectedPlatform} onSelectedPlatform={setSelectedPlatform} />
+          <GameGrid selectedGenre={selectedGenre} selectedPlatform={selectedPlatform} />
         </GridItem>
       </Grid>
     </>
