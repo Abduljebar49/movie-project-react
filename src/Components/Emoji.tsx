3b6cec57290383eeb9c1609610b353d833529860
Abduljebar <Abduljebar49@gmail.com>
2023-12-02 15:31:53 +0300

    emoji added

---
+++ src/Components/Emoji.tsx
@@ -0,0 +1,20 @@
+import bullsEye from '../assets/Emojis/bulls-eye.webp';
+import thumbsUp from '../assets/Emojis/thumbs-up.webp';
+import meh from '../assets/Emojis/meh.webp';
+import { Image, ImageProps } from '@chakra-ui/react';
+
+interface Props{
+    rating:number
+}
+const Emoji = ({rating}:Props) => {
+    const emojiMap:{[key:number]:ImageProps} = {
+        3: {boxSize:'25px',src: meh, alt: 'meh'},
+        4: {boxSize:'25px',src: thumbsUp, alt: 'recommended'},
+        5: {boxSize:'35px',src: bullsEye, alt: 'exceptional'},
+    }
+  return (
+    <Image {...emojiMap[rating]} marginTop={1} />
+  )
+}
+
+export default Emoji
